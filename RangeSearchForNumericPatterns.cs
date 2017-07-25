5ae549c9baf5aa9e54c2b2acffe884a654210f0d
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2017-07-26 02:04:40 +0300

    Adding getDataWStartEndPttrn() overload for including some more text after the

--- RangeSearchForNumericPatterns.cs
+++ RangeSearchForNumericPatterns.cs
@@ -8,33 +8,60 @@ public class Program
     {
         StringBuilder sb = new StringBuilder();
 
-        sb.Append("0009\n");
-        sb.Append("0010\n");
-        sb.Append("0015\n");
-        sb.Append("0019\n");
-        sb.Append("0025\n");
-        sb.Append("0027\n");
-        sb.Append("0029\n");
-        sb.Append("0031\n");
-        sb.Append("0034\n");
-        sb.Append("0035\n");
-        sb.Append("0039\n");
+        //sb.Append("0009\n");
+        //sb.Append("0010\n");
+        //sb.Append("0015\n");
+        //sb.Append("0019\n");
+        //sb.Append("0025\n");
+        //sb.Append("0027\n");
+        //sb.Append("0029\n");
+        //sb.Append("0031\n");
+        //sb.Append("0034\n");
+        //sb.Append("0035\n");
+        //sb.Append("0039\n");
 
-        string r1 = Program.getDataWStartEndPttrn(sb, "0012", "0028");
-        string r2 = Program.getDataWStartEndPttrn(sb, "0012", "0030");
-        string r3 = Program.getDataWStartEndPttrn(sb, "0010", "0029");
-        string r4 = Program.getDataWStartEndPttrn(sb, "0008", "0020");
-        string r5 = Program.getDataWStartEndPttrn(sb, "001", "002");
+        //string r1 = Program.getDataWStartEndPttrn(sb, "0012", "0028");
+        //string r2 = Program.getDataWStartEndPttrn(sb, "0012", "0030");
+        //string r3 = Program.getDataWStartEndPttrn(sb, "0010", "0029");
+        //string r4 = Program.getDataWStartEndPttrn(sb, "0008", "0020");
+        //string r5 = Program.getDataWStartEndPttrn(sb, "001", "002");
+        //string r6 = Program.getDataWStartEndPttrn(sb, "01", "002");
+
+        //Console.WriteLine(r1);
+        //Console.WriteLine("---------");
+        //Console.WriteLine(r2);
+        //Console.WriteLine("---------");
+        //Console.WriteLine(r3);
+        //Console.WriteLine("---------");
+        //Console.WriteLine(r4);
+        //Console.WriteLine("---------");
+        //Console.WriteLine(r5);
+        //Console.WriteLine("---------");
+        //Console.WriteLine(r6);
+
+
+
+        sb.Append("13:08:30:5276604 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+        sb.Append("13:08:30:5736962 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+        sb.Append("13:08:30:6227343 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+        sb.Append("13:08:30:6757752 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+        sb.Append("13:08:30:7208103 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+        sb.Append("13:08:30:7668739 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+        sb.Append("13:08:30:8129079 Main: 41044 - 48.7617 M-- Other PIDS 2 - 79.1016 M\n");
+
+        string r1 = Program.getDataWStartEndPttrn(sb, "13:08:30:6", "13:08:30:7", "\n");
+        string r2 = Program.getDataWStartEndPttrn(sb, "13:08:30:5", "13:08:30:6", "\n");
+
 
         Console.WriteLine(r1);
         Console.WriteLine("---------");
         Console.WriteLine(r2);
         Console.WriteLine("---------");
-        Console.WriteLine(r3);
-        Console.WriteLine("---------");
-        Console.WriteLine(r4);
-        Console.WriteLine("---------");
-        Console.WriteLine(r5);
+
+
+
+
+
 
         Console.ReadLine();
 
@@ -84,6 +111,7 @@ public class Program
         // add appropriate amount of zeros
         string finalS = subI.ToString().PadLeft(pttrn.Length, '0');
 
+        // return
         return finalS;
     }
 
@@ -102,12 +130,13 @@ public class Program
         int subI;
         Int32.TryParse(subS, out subI);
 
-        // decrement by 1
+        // increment by 1
         subI -= 1;
 
         // add appropriate amount of zeros
         string finalS = subI.ToString().PadLeft(pttrn.Length, '0');
 
+        // return
         return finalS;
     }
 
@@ -118,12 +147,33 @@ public class Program
 
         int si = getStartIdx(s, strPttr);
         int se = getEndIdx(s, endPttr);
-        
-        // get the subpattern by the lastIndexOf() it
-        // + endPttr.Length to include the pattern itself
+
         s = s.Substring(si, se - si + endPttr.Length).TrimEnd('\n');
 
         return s;
+    }
+
+    static internal string getDataWStartEndPttrn(StringBuilder data, string strPttr, string endPttr, string extendedTerminator)
+    {
+        string s = data.ToString();
+
+        int si = getStartIdx(s, strPttr);
+        int se = getEndIdx(s, endPttr);
+
+        int sefinal = se - si + endPttr.Length;
+
+        string s1 = s.Substring(si, se - si);
+
+        // do not discard the rest of the string
+        // right away, use it for getting the
+        // extendedTerminator possition
+        string sTMP = s.Substring(se);
+
+        // get the full last line starting from endPttr, 
+        // untill and including the extendedTerminator
+        string s2 = s.Substring(se, sTMP.IndexOf(extendedTerminator));
+
+        return s1 + s2;
     }
 
 }
